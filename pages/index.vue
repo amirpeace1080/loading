1c8fac9f08e41808062f522271298ab111618883
amirpeace1080 <amirpeace1080@gmail.com>
2020-11-23 12:44:36 +0330

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <h1> {{name }} Page </h1>
+        <div class="container">
+            <nuxt-link to="/about">About Page</nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return new Promise((resolve) =>{
+      setTimeout(() => {
+        resolve({ name : 'About'})
+      }, 1000);
+    })
+  },
+}
+</script>
+
+
+<style scoped>
+.container {
+  font-size: 20px;
+  text-align: center;
+  padding-top: 100px;
+}
+</style>
